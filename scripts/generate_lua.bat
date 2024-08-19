91a56fec82176c3ae673b72e4c396476adc4089f
Steve Vermeulen <sfvermeulen@gmail.com>
2024-08-19 22:33:37 +0800

    Added asserts.lua, trace_entry.lua, tracing.lua. Changed generate_lua.bat.

--- scripts/generate_lua.bat
+++ scripts/generate_lua.bat
@@ -6,6 +6,9 @@ rmdir /s /q gen
 mkdir gen
 mkdir gen\tea_leaves
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\args_parser.tl -o %~dp0\..\gen\tea_leaves\args_parser.lua
+call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\asserts.tl -o %~dp0\..\gen\tea_leaves\asserts.lua
+call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\class.tl -o %~dp0\..\gen\tea_leaves\class.lua
+call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\class.tl -o %~dp0\..\gen\tea_leaves\class.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\constants.tl -o %~dp0\..\gen\tea_leaves\constants.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\document.tl -o %~dp0\..\gen\tea_leaves\document.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\document_manager.tl -o %~dp0\..\gen\tea_leaves\document_manager.lua
@@ -19,6 +22,8 @@ call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\server_state.tl -o %~dp0\..
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\stdin_reader.tl -o %~dp0\..\gen\tea_leaves\stdin_reader.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\teal_project_config.tl -o %~dp0\..\gen\tea_leaves\teal_project_config.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\tl_helper.tl -o %~dp0\..\gen\tea_leaves\tl_helper.lua
+call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\trace_entry.tl -o %~dp0\..\gen\tea_leaves\trace_entry.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\trace_stream.tl -o %~dp0\..\gen\tea_leaves\trace_stream.lua
+call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\tracing.tl -o %~dp0\..\gen\tea_leaves\tracing.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\uri.tl -o %~dp0\..\gen\tea_leaves\uri.lua
 call luarocks\bin\tl.bat gen %~dp0\..\src\tea_leaves\util.tl -o %~dp0\..\gen\tea_leaves\util.lua
